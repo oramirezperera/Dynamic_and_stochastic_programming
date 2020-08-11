2e6fbf41bcc6a62e9d940de11994c845526efcc5
Orlando Ramirez <ramirez.orlando17@gmail.com>
2020-08-11 11:14:12 -0400

    created a the fibonacci recursive program

--- dynamic_programming.py
+++ dynamic_programming.py
@@ -1,5 +1,10 @@
-def recursive_fibonacci():
-    pass
+def recursive_fibonacci(n):
+    if n == 0 or n == 1:
+        return 1
+    return recursive_fibonacci(n - 1) + recursive_fibonacci(n - 2)
 
 if __name__ == '__main__':
-    pass
+    n = int(input('Please, write a number: '))
+
+    result = recursive_fibonacci(n)
+    print(result)
