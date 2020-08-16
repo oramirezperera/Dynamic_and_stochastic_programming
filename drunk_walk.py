0e67ec7ccf92b4085082c409a26ac0ad131a8b8b
Orlando Ramirez <ramirez.orlando17@gmail.com>
2020-08-16 12:24:30 -0400

    fixed some typos

--- drunk_walk.py
+++ drunk_walk.py
@@ -9,9 +9,9 @@ class Drunk: #Base class so we can generate different implementations
 class TraditionalDrunk(Drunk): #Drunk as a parameter means it's an amplification of drunk
     
     def __init__(self, name):
-        super().__ini__(name) #Here you got the reference to the superclass
+        super().__init__(name) #Here you got the reference to the superclass
     
-    def walk(): #this drunk man walks randomly through four options
+    def walk(self): #this drunk man walks randomly through four options
         return random.choice([(0, 1), (0, -1), (1, 0), (-1, 0)]) # random.choice picks one value of this list randomly
         #This implementation gives you the chance to change the probabilities of any value
-        
+
