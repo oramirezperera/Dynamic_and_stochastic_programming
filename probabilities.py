87f39880180e605e58188f590e544205320e8316
Orlando Ramirez <ramirez.orlando17@gmail.com>
2020-08-27 16:29:04 -0400

    now you can get the probability of getting at least one 1

--- probabilities.py
+++ probabilities.py
@@ -18,6 +18,14 @@ def main(number_of_throws, number_of_tries):
         throw_sequence = throw_dice(number_of_throws)
         throws.append(throw_sequence)
 
+    throws_with_1 = 0
+
+    for throw in throws:
+        if 1 in throw:
+            throws_with_1 += 1
+
+    probability_of_throws_with_1 = throws_with_1 / number_of_tries
+    print(f'The probability to get at least one 1 in {number_of_tries} is {probability_of_throws_with_1}')
 
 if __name__ == '__main__':
     number_of_throws = int(input('How many dice throws do you want? '))
