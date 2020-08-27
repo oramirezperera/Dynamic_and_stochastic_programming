a20c3354e8f699cee4c9d1efc0c77ba909945b2a
Orlando Ramirez <ramirez.orlando17@gmail.com>
2020-08-27 16:15:21 -0400

    created the entry point and the first two variables

---
+++ probabilities.py
@@ -0,0 +1,8 @@
+import random
+
+
+if __name__ == '__main__':
+    number_of_throws = int(input('How many dice throws do you want? '))
+    number_of_tries = int(input('How many tries do you want? '))
+
+    
