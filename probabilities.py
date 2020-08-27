eb4644dd59396219b59967f2a7b47d4226db4324
Orlando Ramirez <ramirez.orlando17@gmail.com>
2020-08-27 17:15:16 -0400

    now changes to the probabilities file, now has to dices and calculates the probabilities of getting 12

--- probabilities.py
+++ probabilities.py
@@ -2,11 +2,13 @@ import random
 
 
 def throw_dice(number_of_throws):
-    throw_sequence = []
+    throw_sequence = [] 
 
     for _ in range(number_of_throws):
         throw = random.choice([1, 2, 3, 4, 5, 6]) #it could be randint(1,7) too 
-        throw_sequence.append(throw)
+        throw2 = random.choice([1, 2, 3, 4, 5, 6])
+        result = throw + throw2
+        throw_sequence.append(result)
 
     return throw_sequence
 
@@ -18,14 +20,14 @@ def main(number_of_throws, number_of_tries):
         throw_sequence = throw_dice(number_of_throws)
         throws.append(throw_sequence)
 
-    throws_with_1 = 0
+    throws_with_12 = 0
 
     for throw in throws:
-        if 1 not in throw:
-            throws_with_1 += 1
+        if 12 in throw:
+            throws_with_12 += 1
 
-    probability_of_throws_with_1 = throws_with_1 / number_of_tries
-    print(f'The probability to not get at least one 1 in {number_of_tries} is {probability_of_throws_with_1}')
+    probability_of_throws_with_12 = throws_with_12 / number_of_tries
+    print(f'The probability to not get at least one 12 in {number_of_tries} is {probability_of_throws_with_12}')
 
 if __name__ == '__main__':
     number_of_throws = int(input('How many dice throws do you want? '))
