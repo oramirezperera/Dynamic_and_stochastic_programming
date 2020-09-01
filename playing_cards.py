2e757fb07f2c5f83e11db7d16fa3e14b1ba570bd
Orlando Ramirez <ramirez.orlando17@gmail.com>
2020-09-01 12:05:30 -0400

    changes to the cards program, now can calculate if it is a straight

--- playing_cards.py
+++ playing_cards.py
@@ -3,7 +3,7 @@ import collections
 from enum import IntEnum
 
 TYPES = ['spades', 'hearts', 'diamonds', 'clubs']
-VALUES = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+VALUES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13] #1 represents A, 11, 12, and 13 represents J, Q, K
 
 
 def create_card(): #this function is to create the playing cards with the two list above
@@ -38,19 +38,51 @@ def main(hand_size, tries):
             values.append(card[1])
 
         counter = dict(collections.Counter(values)) #Counter is a collections class 
+        
         for val in counter.values():
             if val == 2:
                 pairs += 1
                 break
-       
-            
+    
+
+           
     pair_probability = pairs / tries
     print(f'The probability of getting a pair in one hand of {hand_size} playing cards is {pair_probability}')
+    
 
+
+def is_straight(hand_size, tries):
+
+    playing_card2 = create_card()
+    hands = []
+
+    for _ in range(tries):
+        straight_hand = get_hand(playing_card2, hand_size)
+        hands.append(straight_hand)
+
+    straights = 0
+
+    for hand in hands:
+        values = []
+        for card in hand:
+            values.append(card[1])
+        counter = dict(collections.Counter(values))
+        sorted_values = sorted(values)
+
+        i = 0 
+        while((sorted_values[i] - sorted_values[i+1]) == -1): #this checks if it's a straight
+            i += 1
+            if i == len(sorted_values) - 1:
+                straights += 1
+                break
+    
+    straights_probability = straights / tries
+    print(f'The probability of getting a straight in one hand of {hand_size} is {straights_probability}')
+        
 
 
 if __name__ == '__main__':
     hand_size = int(input('How many cards are the hands in this game? '))
     tries = int(input('How many tries to calculate the probability? '))
     main(hand_size, tries)
-   
+    is_straight(hand_size, tries)
